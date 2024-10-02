f930d3c5013238d3ede973ec219c412df4fd6bea
Girum-Tamirat <girumtamirat10@gmail.com>
2024-10-02 19:05:57 +0300

    fixedabs

--- myApp.js
+++ myApp.js
@@ -3,8 +3,8 @@ let app = express();
 app.get('/', (req,res)=>{
     res.send('Hello Express');
 });
-const absolutePath=__dirname + '/views/index.html';
 app.get('/', (req,res)=>{
+    const absolutePath = path.join(__dirname, 'views', 'index.html');
     res.sendFile(absolutePath);
 });
 
