72f61531f418121fea3c73d38393285dd52c507b
Girum-Tamirat <girumtamirat10@gmail.com>
2024-10-02 19:00:15 +0300

    dirname

--- myApp.js
+++ myApp.js
@@ -3,9 +3,8 @@ let app = express();
 app.get('/', (req,res)=>{
     res.send('Hello Express');
 });
-console.log("Hello World");
 app.get('/', (req,res)=>{
-    res.sendFile('/views/index.html');
+    res.sendFile(__dirname+'/views/index.html');
 });
 
 
